41b26f9b17659d8f3961d019b6978ef8d2dbb3c7
Kim Hunter <bigkm1@gmail.com>
2012-11-24 15:35:04 +1000

    update docgen script for new path

--- script/gendoc.sh
+++ script/gendoc.sh
@@ -1,1 +1,1 @@
-appledoc  --project-name=OMAlertView --project-company=bigkm --company-id=com.github.bigkm --output=docs --index-desc=README.markdown src/
+appledoc  --project-name=OMAlertView --project-company=bigkm --company-id=com.github.bigkm --output=docs --index-desc=README.markdown OMAlertView
